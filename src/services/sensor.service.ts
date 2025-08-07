f1206d648d34d3cedc8cab3327bd35ce15311e17
Della Duck <aurelien.strimelle@bstorm.be>
2025-08-07 17:17:06 +0200

    Mapping sensor v2 🦟

--- src/services/sensor.service.ts
+++ src/services/sensor.service.ts
@@ -6,11 +6,13 @@ export async function getCurrentSensors(): Promise<Sensor[] | null> {
     //! ↓ Ne pas faire ça en prod. Merci d'avance. Bisous
     await (new Promise(resolve => setTimeout(resolve, 500)));
 
-    const { data } = await axios.get<SensorRequestData>('/api/sensors/current', { 
-        baseURL: 'http://localhost:3000' 
+    const { data } = await axios.get<SensorRequestData>('/api/sensors/current', {
+        baseURL: 'http://localhost:3000'
     });
 
-    // Mapping de "SensorRequestData" vers "Sensor"
+    //! Mapping de "SensorRequestData" vers "Sensor"
+    //* - Générique
+    /*
     const result: Sensor[] = [];
     for (const [key, content] of Object.entries(data)) {
         result.push({
@@ -20,6 +22,21 @@ export async function getCurrentSensors(): Promise<Sensor[] | null> {
         });
     }
     return result;
+    */
+
+    //* - Customisé par ressource 
+    return [
+        {
+            sensor: 'Humidity',
+            value: (data.humidity.value / 100).toLocaleString('fr-be', { style: 'percent', minimumSignificantDigits: 2 }),
+            timestamp: new Date(data.humidity.timestamp)
+        },
+        {
+            sensor: 'Temperature',
+            value: data.temperature.value.toLocaleString('fr-be', { style: 'unit', unit: 'celsius' }),
+            timestamp: new Date(data.temperature.timestamp)
+        }
+    ]
 }
 
 
